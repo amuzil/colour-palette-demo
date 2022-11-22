76cd0ee0c09521aa78c091020af1ac8ed185b59b
Luka Leer <mahtaran@amuzil.com>
2022-11-22 23:24:08 +0000

    🚑 Fix broken devcontainer configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function HomePage() {
 	return (
-		<div className="w-screen h-screen flex justify-center items-center">
+		<div className="w-screen h-screen flex flex-col justify-center items-center">
 			<h1 className="text-3xl text-center font-bold">Home Page</h1>
+			<p className="text-center">This is the home page</p>
 		</div>
 	);
 }
